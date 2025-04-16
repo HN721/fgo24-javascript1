6cd3729bd9c8b21ec99ceda16f742833438106e6
Hosea Nainggolan <119390246+HN721@users.noreply.github.com>
2025-04-16 13:35:52 +0700

    Update for-loop.js

--- for-loop.js
+++ for-loop.js
@@ -1,7 +1,31 @@
-for (let i = 1; i <= 5; i++) {
-  if (i === 4) {
-    console.log("fazztrack");
-  } else {
-    console.log(i);
+// for (let i = 1; i <= 5; i++) {
+//   if (i === 4) {
+//     console.log("Fazztrack");
+//   } else {
+//     console.log(i);
+//   }
+// }
+// let x = 0;
+// let y = 2;
+// while (x < 5) {
+// y =
+//   x++;
+// }
+
+// for (let i = 1; i <= 5; i++) {
+//   for (let y = 0; y < 5; y++) {
+//     console.log("Hello");
+//   }
+//   console.log("end of loop");
+// }
+let x = 5;
+while (x > 0) {
+  let tempp = "";
+  let y = 0;
+  while (y < x) {
+    tempp += "*";
+    y++;
   }
+  console.log(tempp);
+  x--;
 }
