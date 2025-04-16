2120c70eea4bee4fcc8561e0bf451fbb9bbdb463
Hosea Nainggolan <119390246+HN721@users.noreply.github.com>
2025-04-16 14:41:15 +0700

    Update for-loop.js

--- for-loop.js
+++ for-loop.js
@@ -1,38 +1,81 @@
-// for (let i = 1; i <= 5; i++) {
-//   if (i === 4) {
-//     console.log("Fazztrack");
-//   } else {
-//     console.log(i);
-//   }
-// }
-// let x = 0;
-// let y = 2;
-// while (x < 5) {
-// y =
-//   x++;
-// }
-
-// for (let i = 1; i <= 5; i++) {
-//   for (let y = 0; y < 5; y++) {
-//     console.log("Hello");
-//   }
-//   console.log("end of loop");
-// }
-let y = 0
-while(y<5){
-  let x = 0 
-  let row = ""
-  while(x<5-y){
-    row += "* "
-    x++
+console.log("loop while");
+let u = 1;
+while (u <= 5) {
+  if (u === 4) {
+    console.log("Fazztrack");
+  } else {
+    console.log(u);
   }
-  console.log(row)
-  y++}
-  
-let x = 0;
+  u++;
+}
+let s = 5;
+while (s > 0) {
+  let tempp = "";
+  let y = 0;
+  while (y < s) {
+    tempp += "*";
+    y++;
+  }
+  console.log(tempp);
+  s--;
+}
+let k = 5;
 let tempp = "";
-while (x < 5) {
+while (k > 0) {
   tempp += "*";
   console.log(tempp);
-  x++;
+  k--;
 }
+console.log("loop for");
+for (let i = 0; i < 5; i++) {
+  let tempp = "";
+  for (let y = 0; y < i; y++) {
+    tempp += "*";
+  }
+  console.log(tempp);
+}
+for (let i = 1; i <= 5; i++) {
+  if (i === 4) {
+    console.log("Fazztrack");
+  } else {
+    console.log(i);
+  }
+}
+for (let i = 5; i >= 0; i--) {
+  let tempp = "";
+  for (let y = 0; y < i; y++) {
+    tempp += "*";
+  }
+  console.log(tempp);
+}
+console.log("loop do while");
+let x = 0;
+do {
+  if (x === 4) {
+    console.log("Fazztrack");
+  } else {
+    console.log(x);
+  }
+  x++;
+} while (x <= 5);
+
+let i = 0;
+
+do {
+  let tempp = "";
+  for (let y = 0; y < i; y++) {
+    tempp += "*";
+  }
+  console.log(tempp);
+  i++;
+} while (i <= 5);
+
+let j = 5;
+do {
+  let tempp = "";
+  for (let y = 0; y < j; y++) {
+    tempp += "*";
+  }
+  console.log(tempp);
+  j--;
+} while (j >= 0);
